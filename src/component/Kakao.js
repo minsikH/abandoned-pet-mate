21cdd88cd1eb9e9d7b57d93d5b1b1e32e26626b3
minsikH <qwerasdfzxcv353535@gmail.com>
2024-06-13 06:05:43 +0900

    edit

--- src/component/Kakao.js
+++ src/component/Kakao.js
@@ -127,90 +127,92 @@ import React, { useEffect } from "react";
 const { kakao } = window;
 
 const Kakao = ({ careNm, kindCd, careAddr }) => {
-    const isDog = kindCd.includes("[개]");
-    const isCat = kindCd.includes("[고양이]");
+  const isDog = kindCd.includes("[개]");
+  const isCat = kindCd.includes("[고양이]");
 
-    useEffect(() => {
-        const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-        const options = {
-            //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-            level: 3, //지도의 레벨(확대, 축소 정도)
+  useEffect(() => {
+    const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(36.5, 127.5), // 전국 중심 좌표 설정
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+    //console.log("ki4nd", kindCd);
+    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    // 주소를 좌표로 변환하는 geocoder 객체 생성
+    const geocoder = new kakao.maps.services.Geocoder();
 
+    // 주소를 좌표로 변환하여 지도에 표시하는 함수
+    geocoder.addressSearch(careAddr, function (result, status) {
+      // 주소 검색 완료 시
+      if (status === kakao.maps.services.Status.OK) {
+        const coords = new kakao.maps.LatLng(result[0].y, result[0].x); // 좌표를 LatLng 객체로 변환
+        // 마커 이미지 정보 설정
+        const imageSize = new kakao.maps.Size(50, 50);
+        // 마커 이미지 설정
+        let imageSrc;
+
+        if (isDog) {
+          imageSrc = process.env.PUBLIC_URL + "/img/icon_dog_marker.png"; // 개 이미지 경로
+        } else if (isCat) {
+          imageSrc = process.env.PUBLIC_URL + "/img/icon_cat_marker.png"; // 고양이 이미지 경로
+        } else {
+          imageSrc = process.env.PUBLIC_URL + "/img/icon_no_marker.png"; // 기본 이미지 경로
+        }
+        const imageOption = { offset: new kakao.maps.Point(27, 69) };
+        const markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imageOption
+        );
+        const marker = new kakao.maps.Marker({
+          map: map,
+          position: coords,
+          image: markerImage,
+          clickable: false, // 마커 클릭 이벤트 비활성화
+        }); // 마커 생성 및 지도에 표시
+        map.setCenter(coords); // 지도 중심 좌표를 검색한 위치로 이동
+
+        // 마커 지도에 추가
+        marker.setMap(map);
+
+        // 클릭 이벤트 처리
+        const handleClick = () => {
+          // 클릭된 위치의 좌표
+          const destinationCoords = coords.getLat() + "," + coords.getLng();
+          // 카카오맵 URL
+          const kakaoMapURL = `https://map.kakao.com/link/to/${careNm},${destinationCoords}`;
+          // 새 탭에서 카카오맵 페이지 열기
+          window.open(kakaoMapURL, "_blank");
         };
-        //console.log("ki4nd", kindCd);
-        const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-        // 주소를 좌표로 변환하는 geocoder 객체 생성
-        const geocoder = new kakao.maps.services.Geocoder();
+        // 전역 범위에서 함수 사용 가능하도록 선언
+        window.handleClick = handleClick;
+        // 커스텀 오버레이 생성
+        const imagePath = process.env.PUBLIC_URL + "/img/d_arrow_b.png";
+        const content =
+          '<div class="customoverlay">' +
+          `<span class="title" onclick="handleClick()">${careNm}<img src="${imagePath}" alt="" /></span>` +
+          "</div>";
 
-        // 주소를 좌표로 변환하여 지도에 표시하는 함수
-        geocoder.addressSearch(careAddr, function (result, status) {
-            // 주소 검색 완료 시
-            if (status === kakao.maps.services.Status.OK) {
-                const coords = new kakao.maps.LatLng(result[0].y, result[0].x); // 좌표를 LatLng 객체로 변환
-                // 마커 이미지 정보 설정
-                const imageSize = new kakao.maps.Size(50, 50);
-                // 마커 이미지 설정
-                let imageSrc;
-
-                if (isDog) {
-                    imageSrc = process.env.PUBLIC_URL + "/img/icon_dog_marker.png"; // 개 이미지 경로
-                } else if (isCat) {
-                    imageSrc = process.env.PUBLIC_URL + "/img/icon_cat_marker.png"; // 고양이 이미지 경로
-                } else {
-                    imageSrc = process.env.PUBLIC_URL + "/img/icon_no_marker.png"; // 기본 이미지 경로
-                }
-                const imageOption = { offset: new kakao.maps.Point(27, 69) };
-                const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-                const marker = new kakao.maps.Marker({
-                    map: map,
-                    position: coords,
-                    image: markerImage,
-                    clickable: false // 마커 클릭 이벤트 비활성화
-                }); // 마커 생성 및 지도에 표시
-                map.setCenter(coords); // 지도 중심 좌표를 검색한 위치로 이동
-
-                // 마커 지도에 추가
-                marker.setMap(map);
-
-
-                // 클릭 이벤트 처리
-                const handleClick = () => {
-                    // 클릭된 위치의 좌표
-                    const destinationCoords = coords.getLat() + "," + coords.getLng();
-                    // 카카오맵 URL
-                    const kakaoMapURL = `https://map.kakao.com/link/to/${careNm},${destinationCoords}`;
-                    // 새 탭에서 카카오맵 페이지 열기
-                    window.open(kakaoMapURL, "_blank");
-                };
-                // 전역 범위에서 함수 사용 가능하도록 선언
-                window.handleClick = handleClick;
-                // 커스텀 오버레이 생성
-                const imagePath = process.env.PUBLIC_URL + "/img/d_arrow_b.png";
-                const content =
-                '<div class="customoverlay">' +
-                `<span class="title" onclick="handleClick()">${careNm}<img src="${imagePath}" alt="" /></span>` +
-                "</div>";
-
-                // 커스텀 오버레이 생성
-                /* const content = '<div class="customoverlay">' + `<span class="title">${careNm}</span>` + "</div>"; */
-                const position = coords; // 좌표값으로 설정
-                const customOverlay = new kakao.maps.CustomOverlay({
-                    map: map,
-                    position: position,
-                    content: content,
-                    xAnchor: 0.5,
-                    yAnchor: 4.5,
-                });
-            }
+        // 커스텀 오버레이 생성
+        /* const content = '<div class="customoverlay">' + `<span class="title">${careNm}</span>` + "</div>"; */
+        const position = coords; // 좌표값으로 설정
+        const customOverlay = new kakao.maps.CustomOverlay({
+          map: map,
+          position: position,
+          content: content,
+          xAnchor: 0.5,
+          yAnchor: 4.5,
         });
-    }, [careAddr]);
+      }
+    });
+  }, [careAddr]);
 
-    return (
-        <div className="kakao">
-            <div id="map"></div>
-        </div>
-    );
+  return (
+    <div className="kakao">
+      <div id="map"></div>
+    </div>
+  );
 };
 
 export default Kakao;
--- src/component/KakaoShelter.js
+++ src/component/KakaoShelter.js
@@ -70,77 +70,89 @@ import React, { useEffect, useRef } from "react";
 const { kakao } = window;
 
 const KakaoShelter = ({ uniqueAllPetsList, onShelterClick }) => {
-    const mapRef = useRef(null);
+  const mapRef = useRef(null);
 
-    useEffect(() => {
-        //console.log("uniqueAllPetsList:", uniqueAllPetsList);
-        const container = document.getElementById("map_shelter");
-        const options = {
-            center: new kakao.maps.LatLng(36.5, 127.5), // 전국 중심 좌표 설정
-            level: 12, // 초기 확대 레벨 설정
-        };
+  useEffect(() => {
+    //console.log("uniqueAllPetsList:", uniqueAllPetsList);
+    const container = document.getElementById("map_shelter");
+    const options = {
+      center: new kakao.maps.LatLng(36.5, 127.5), // 전국 중심 좌표 설정
+      level: 12, // 초기 확대 레벨 설정
+    };
 
-        const map = new kakao.maps.Map(container, options);
-        mapRef.current = map;
+    const map = new kakao.maps.Map(container, options);
+    mapRef.current = map;
 
-        // uniqueAllPetsList가 배열이 아니라 객체일 경우 처리
-        /*         const petItems = uniqueAllPetsList?.response?.body?.items?.item; */
-        /*         console.log("petItems:", petItems); */
-        if (Array.isArray(uniqueAllPetsList)) {
-            uniqueAllPetsList.forEach((pet) => {
-                const { careNm, kindCd, careAddr, desertionNo,careTel } = pet;
+    // uniqueAllPetsList가 배열이 아니라 객체일 경우 처리
+    /*         const petItems = uniqueAllPetsList?.response?.body?.items?.item; */
+    /*         console.log("petItems:", petItems); */
+    if (Array.isArray(uniqueAllPetsList)) {
+      uniqueAllPetsList.forEach((pet) => {
+        const { careNm, kindCd, careAddr, desertionNo, careTel } = pet;
 
-                const geocoder = new kakao.maps.services.Geocoder();
+        const geocoder = new kakao.maps.services.Geocoder();
 
-                geocoder.addressSearch(careAddr, function (result, status) {
-                    if (status === kakao.maps.services.Status.OK) {
-                        const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-                        const imageSize = new kakao.maps.Size(30, 30);
-                        const imageSrc = process.env.PUBLIC_URL + "/img/icon_shelter.png";
-                        const imageOption = { offset: new kakao.maps.Point(27, 69) };
-                        const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-                        const marker = new kakao.maps.Marker({
-                            map: map,
-                            position: coords,
-                            image: markerImage,
-                        });
-
-                        // 클릭 이벤트 처리
-                        const handleClick = () => {
-                            // 클릭된 보호소 정보를 콜백 함수로 전달
-                            onShelterClick({ careNm, kindCd, careAddr, desertionNo,careTel });
-                        };
-
-                        // 이 부분이 중요합니다. 클릭 이벤트를 설정해줘야 합니다.
-                        marker.addListener("click", handleClick);
-
-                        const content =
-                            '<div class="customoverlay">' + `<span class="title">${careNm}</span>` + "</div>";
-
-                        const position = coords;
-                        const customOverlay = new kakao.maps.CustomOverlay({
-                            map: map,
-                            position: position,
-                            content: content,
-                            xAnchor: 0.5,
-                            yAnchor: 4.5,
-                        });
-                    }
-                });
+        geocoder.addressSearch(careAddr, function (result, status) {
+          if (status === kakao.maps.services.Status.OK) {
+            const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            const imageSize = new kakao.maps.Size(30, 30);
+            const imageSrc = process.env.PUBLIC_URL + "/img/icon_shelter.png";
+            const imageOption = { offset: new kakao.maps.Point(27, 69) };
+            const markerImage = new kakao.maps.MarkerImage(
+              imageSrc,
+              imageSize,
+              imageOption
+            );
+            const marker = new kakao.maps.Marker({
+              map: map,
+              position: coords,
+              image: markerImage,
             });
-        }
 
-        // 보호소 클릭 시 이동 이벤트 막기
-        kakao.maps.event.addListener(map, "click", function () {
-            map.setCenter(mapRef.current.getCenter());
+            // 클릭 이벤트 처리
+            const handleClick = () => {
+              // 클릭된 보호소 정보를 콜백 함수로 전달
+              onShelterClick({
+                careNm,
+                kindCd,
+                careAddr,
+                desertionNo,
+                careTel,
+              });
+            };
+
+            // 이 부분이 중요합니다. 클릭 이벤트를 설정해줘야 합니다.
+            marker.addListener("click", handleClick);
+
+            const content =
+              '<div class="customoverlay">' +
+              `<span class="title">${careNm}</span>` +
+              "</div>";
+
+            const position = coords;
+            const customOverlay = new kakao.maps.CustomOverlay({
+              map: map,
+              position: position,
+              content: content,
+              xAnchor: 0.5,
+              yAnchor: 4.5,
+            });
+          }
         });
-    }, [uniqueAllPetsList]);
+      });
+    }
 
-    return (
-        <div className="kakao">
-            <div id="map_shelter"></div>
-        </div>
-    );
+    // 보호소 클릭 시 이동 이벤트 막기
+    kakao.maps.event.addListener(map, "click", function () {
+      map.setCenter(mapRef.current.getCenter());
+    });
+  }, [uniqueAllPetsList]);
+
+  return (
+    <div className="kakao">
+      <div id="map_shelter"></div>
+    </div>
+  );
 };
 
 export default KakaoShelter;
