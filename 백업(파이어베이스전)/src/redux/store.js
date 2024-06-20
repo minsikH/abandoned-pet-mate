42b3b8a2e142c1569026a61a1eccf06cad958bf1
minsikH <155708917+minsikH@users.noreply.github.com>
2024-06-20 14:24:27 +0900

    Delete 백업(파이어베이스전) directory

--- 백업(파이어베이스전)/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import rootReducer from "./reducers";
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-//console.log("Redux state:", store.getState());
-
-export default store;
